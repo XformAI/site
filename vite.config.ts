285d5def19ae8748b6a0a8fd0f24f61e5f5003e1
Jigyansha Jeevan Rout <62542350+sudo-xda@users.noreply.github.com>
2025-04-22 12:05:11 +0530

    r

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,23 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/site' : '/', // ← change to your actual base path
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: mode !== 'production' ? true : false,
+    minify: "esbuild",
   },
 }));
